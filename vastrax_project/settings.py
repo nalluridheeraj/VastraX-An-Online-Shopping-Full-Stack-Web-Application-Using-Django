0fef7b13ff79453ab82ac46e075709e35941c46a
Nalluri Dheeraj <151846209+nalluridheeraj@users.noreply.github.com>
2026-02-18 12:10:14 +0530

    Update settings.py

--- vastrax_project/settings.py
+++ vastrax_project/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
-ALLOWED_HOSTS = ['vastrax.onrender.com']
+ALLOWED_HOSTS = ['*']
+
 
 
 INSTALLED_APPS = [
